7761cafb3b06f95b6f885ed62a38475cb11168c9
Sim Mikhail <msim@sfedu.ru>
2023-10-08 15:43:44 +0300

    Now code print hello in different languages

--- hello.pas
+++ hello.pas
@@ -1,1 +1,34 @@
-﻿## Print('Привет, мир!');
+﻿{
+Вывести привет на определенном языке
+}
+begin
+  var language := ReadInteger($'Choose your language:{newline}1. Russian{newline}2. English{newline}3. French{newline}');
+  
+  case language of
+    1: Print('Привет');
+    2: Print('Hello');
+    3: Print('Bonjour');
+  end;
+end.
+{
+Choose your language:
+1. Russian
+2. English
+3. French
+ 1
+Привет 
+
+Choose your language:
+1. Russian
+2. English
+3. French
+ 2
+Hello 
+
+Choose your language:
+1. Russian
+2. English
+3. French
+ 3
+Bonjour 
+}
